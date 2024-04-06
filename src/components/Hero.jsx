c70ee099a37568109fddd21ecf06fba0302df404
Godspower Similoluwa <gpsimi01@gmail.com>
2024-04-06 01:49:35 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,13 @@
 import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
 import Section from "./Section";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax"
+import { useRef } from "react";
 
 const Hero = () => {
+  const parallaxRef = useRef(null)
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
@@ -11,7 +16,7 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-      <div className="container relative">
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities of AI Chatting with Brainwave
@@ -46,6 +51,13 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
+
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40">
+
+                  </ul>
+                </ScrollParallax>
               </div>
             </div>
           </div>
