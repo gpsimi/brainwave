6eb5c414aca90551ba60e27dc9225e2065196f14
Godspower Similoluwa <gpsimi01@gmail.com>
2024-04-08 17:40:45 +0100

    Update Collaboration.jsx

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -40,10 +40,13 @@ const Collaboration = () => {
                     Try it now
                 </Button>
             </div>
-            <div className="lg:ml-auto xl:w-[38rem]">
+            <div className="lg:ml-auto xl:w-[38rem] mt-4">
                 <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
                     {collabText}
                 </p>
+                <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+
+                </div>
             </div>
         </div>
     </Section>
