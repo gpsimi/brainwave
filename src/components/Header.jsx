19d6ed72a4a5786a96369aae28f8f1cc69f7ceba
Godspower Similoluwa <gpsimi01@gmail.com>
2024-04-09 21:00:36 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
@@ -7,6 +6,7 @@ import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
 
 const Header = () => {
     const pathname = useLocation();
@@ -21,6 +21,7 @@ const Header = () => {
             disablePageScroll();
         }
     };
+
     const handleClick = () => {
         if (!openNavigation) return;
 
@@ -30,13 +31,14 @@ const Header = () => {
 
     return (
         <div
-            className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+            className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
                 }`}
         >
             <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-                <a href="#hero" className="block w-[12rem] xl:mr-8">
+                <a className="block w-[12rem] xl:mr-8" href="#hero">
                     <img src={brainwave} width={190} height={40} alt="Brainwave" />
                 </a>
+
                 <nav
                     className={`${openNavigation ? "flex" : "hidden"
                         } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
@@ -60,9 +62,10 @@ const Header = () => {
 
                     <HamburgerMenu />
                 </nav>
+
                 <a
                     href="#signup"
-                    className="button hidden mr-8 text-n-1/50 transition-color hover:text-n-1 lg:block"
+                    className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
                 >
                     New account
                 </a>
